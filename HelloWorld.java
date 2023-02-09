3326c9c6e19341d5db93a9dba0d55bdd117e727c
sandipawchar <sandipawchar4456@gmail.com>
2023-02-09 11:00:26 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-class HelloWorld
+ class HelloWorld
 {
- public static void main (String[]args)
+     public static void main (String[] args)
 {
-System.out.println("Hello World");
+
+        System.out.println("Hello World");
 }
 }
